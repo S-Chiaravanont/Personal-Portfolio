e8dd2fa1af2a2d18cf29ca7eee2d7cfcc39ea402
Chai Chiaravanont <cchiaravanont@gmail.com>
2023-02-14 00:25:18 +0000

    in progress, added boundary and reset functionality

--- client/pages/playground.jsx
+++ client/pages/playground.jsx
@@ -1,9 +1,10 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 
 export default function Playground(props) {
   const elemRef = useRef(null);
   const dragProps = useRef();
   const contRef = useRef(null);
+  const [boundaryStatus, setBoundaryStatus] = useState(false);
 
   function initialiseDrag(event) {
     const { target, clientX, clientY } = event;
@@ -31,21 +32,25 @@ export default function Playground(props) {
     const xMovement = clientX - dragProps.current.dragStartX + dragProps.current.dragStartLeft - contRef.current.getBoundingClientRect().left;
     let yTranslate;
     const yMovement = dragProps.current.dragStartTop + clientY - dragProps.current.dragStartY - contRef.current.getBoundingClientRect().top;
-    if (left + xMovement < boundary.left) {
-      xTranslate = left - boundary.left;
-    } else if (xMovement >= boundary.right) {
-      xTranslate = boundary.right;
+    if (boundaryStatus) {
+      if (left + xMovement < boundary.left) {
+        xTranslate = left - boundary.left;
+      } else if (xMovement >= boundary.right) {
+        xTranslate = boundary.right;
+      } else {
+        xTranslate = xMovement;
+      }
+      if (top + yMovement < boundary.top) {
+        yTranslate = top - boundary.top;
+      } else if (yMovement > boundary.bottom) {
+        yTranslate = boundary.bottom;
+      } else {
+        yTranslate = yMovement;
+      }
     } else {
       xTranslate = xMovement;
-    }
-    if (top + yMovement < boundary.top) {
-      yTranslate = top - boundary.top;
-    } else if (yMovement > boundary.bottom) {
-      yTranslate = boundary.bottom;
-    } else {
       yTranslate = yMovement;
     }
-
     elemRef.current.style.transform = `translate(${xTranslate}px,
      ${yTranslate}px)`;
   };
@@ -55,17 +60,37 @@ export default function Playground(props) {
     window.removeEventListener('mouseup', stopDragging, false);
   };
 
+  const resetHandle = () => {
+    elemRef.current.style.transform = `translate(${0}px,
+     ${0}px)`;
+  };
+
+  const boundaryHandle = () => {
+    if (boundaryStatus) {
+      setBoundaryStatus(false);
+    } else {
+      setBoundaryStatus(true);
+    }
+  };
+
   return (
     <div className='container'>
       <div className='row flex-wrap'>
         <div className='column-full'>
-          <h1 style={{ color: 'white' }}>Movable DIV</h1>
+          <h1 style={{ color: 'white' }}>DRAGGABLE DIV</h1>
+        </div>
+        <div className='column-full' style={{ textAlign: 'end', marginRight: '5%', marginBottom: '20px' }}>
+          <div style={{ marginBottom: '10px' }}>
+            <input type="checkbox" name="boundary" id="boundary" onChange={boundaryHandle} />
+            <label htmlFor="boundary" style={{ color: 'white' }}> Boundary</label>
+          </div>
+          <div>
+            <input type="reset" value="Reset" onClick={resetHandle} style={{ width: '65px', height: '30px' }} />
+          </div>
         </div>
         <div className='column-full'>
           <div className='pg-moving-div-container' ref={contRef}>
-            <div className='pg-moving-div-box text-center' onMouseDown={initialiseDrag} ref={elemRef} >
-              {/* <h5 className='pg-moving-text'>click here</h5> */}
-            </div>
+            <div className='pg-moving-div-box text-center' onMouseDown={initialiseDrag} ref={elemRef} />
           </div>
         </div>
       </div>
